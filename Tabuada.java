c64b5b9768a8ac3efc6ad9aad733d922945b0865
carolricart <88861539+carolricart@users.noreply.github.com>
2021-08-13 10:49:07 -0300

    Create Tabuada.java
    Semana 1: Faça um programa em Java (Tabuada.java) para imprimir a tabuada do 10 de multiplicação de 1 a 10.

---
+++ Tabuada.java
@@ -0,0 +1,20 @@
+/*Caroline Ricart Chilinque de Carvalho - 0050016632 
+TABUADA DO NÚMERO 10 */
+class Main {
+  public static void main(String[] args) {
+   System.out.println("\nTabuada do 10");
+   for (int N=1; N <=10; N++){ 
+    System.out.printf("10 X %d = %d\n",N,10*N);
+  }
+}}
+
+/* TABUADA DO NÚMERO 1 AO 10*/
+class Main {
+  public static void main(String[] args) {
+  for (int i=1; i <=10; i++){ 
+   System.out.println("\nTabuada do "+i);
+   for (int N=1; N <=10; N++){ 
+    System.out.printf("%d X %d = %d\n",i,N,i*N);
+  }
+}}}
+
